07198f191b1c39f0da7777e9e962ec742695bc89
Pascal Amann <pascal.amann.perso@gmail.com>
2026-06-27 16:07:04 +0200

    config db_SQL ok

--- config/db_sql.php
+++ config/db_sql.php
@@ -8,16 +8,17 @@ class DB_SQL {
     public static function get(): PDO {
         if (self::$pdo === null) {
             self::$pdo = new PDO(
-                "mysql:host=localhost;dbname=fil_rouge;charset=utf8mb4",
+                "mysql:host=localhost;dbname=resto;charset=utf8mb4",
                 "root",
                 "",
                 [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
                 ]
+                
             );
+            
         }
-
         return self::$pdo;
     }
 }
